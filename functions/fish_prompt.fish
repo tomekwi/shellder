de246d5109cf50a1931cd5e848b558c7ac540923
Tomek <tomek@lystable.com>
2016-07-05 23:15:19 +0200

    Get rid of segment separators

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -43,17 +43,9 @@ function prompt_segment -d "Function to draw a segment"
   else
     set fg normal
   end
-  if [ "$current_bg" != 'NONE' -a "$argv[1]" != "$current_bg" ]
-    set_color -b $bg
-    set_color $current_bg
-    echo -n "$segment_separator "
-    set_color -b $bg
-    set_color $fg
-  else
-    set_color -b $bg
-    set_color $fg
-    echo -n " "
-  end
+  set_color -b $bg
+  set_color $fg
+  echo -n " "
   set current_bg $argv[1]
   if [ -n "$argv[3]" ]
     echo -n -s $argv[3] " "
@@ -64,7 +56,6 @@ function prompt_finish -d "Close open segments"
   if [ -n $current_bg ]
     set_color -b normal
     set_color $current_bg
-    echo -n "$segment_separator "
   end
   set -g current_bg NONE
 end
